9a3231e7464f2d1a4aebe4a6197d9e23087c6a4c
Trevor Cickovski <tcickovs@fiu.edu>
2018-01-16 14:14:56 -0500

    Removed debugging print statements

--- EuclideanDistancePlugin.R
+++ EuclideanDistancePlugin.R
@@ -1,24 +1,16 @@
 library('vegan')
 
 input <- function(inputfile) {
-  print("A");
   pc <<- read.csv(inputfile, header = TRUE);
-  print("B");
 }
 
 
 run <- function() {
-  print("C");
   cn <<- pc[,1];
-  print("D");
   pc <<- pc[,-1];
-  print("E");
   pc <<- apply(pc, 1, as.numeric);
-  print("F");
   pc <<- t(pc);
-  print("Computing Euclidean Distance...");
   pc <<- as.matrix(vegdist(pc, method="euclidean"));
-  print("G");
 }
 
 output <- function(outputfile) {
